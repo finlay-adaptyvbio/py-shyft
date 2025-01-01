0a69f0c0a69f3acc8be1eef492c4aba3d8447d1d
finyl <finlay.peterkin@gmail.com>
2025-01-01 16:42:13 +0100

    feat: add regions

---
+++ py_shyft/__init__.py
@@ -0,0 +1,8 @@
+REGIONS = {
+    "AMS": "grpc.ams.shyft.to",
+    "FRA": "grpc.fra.shyft.to",
+    "EU": "grpc.eu.shyft.to",
+    "NY": "grpc.ny.shyft.to",
+    "VA": "grpc.va.shyft.to",
+    "US": "grpc.us.shyft.to",
+}
